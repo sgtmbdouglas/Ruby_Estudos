58d62de8196a17cb80b82b6d6f97ed110a25a99d
DCBB <dc@pop-os.localdomain>
2023-02-07 19:43:00 -0300

    metodos de classe

---
+++ 2-Orientacao a objetos/2-Objetos/account.rb
@@ -0,0 +1,24 @@
+ class Account
+
+    attr_reader :number, :holder
+    # attr_writer :balance
+    attr_accessor : balance
+
+    def initialize(number, holder, balance)
+        @number = number
+        @holder = holder
+        @balance = balance
+    end
+
+    # def number
+    #     @number
+    # end
+
+    # def holder
+    #     @holder
+    # end
+
+    # def holder=(other_holder)
+    #     @holder = other_holder
+    # end
+ end
